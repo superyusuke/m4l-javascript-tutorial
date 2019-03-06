f26100b2c660d7c525d7a2bfa1822e4560fb9f2f
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2019-03-06 17:49:29 +0900

    initial commit

---
+++ entry.js
@@ -0,0 +1,10 @@
+import { console, log } from "/src/log";
+
+const nera = [1,2,3,4]
+
+console.log(nera)
+
+log("___________________________________________________");
+log("Reload:", new Date());
+
+clear()
